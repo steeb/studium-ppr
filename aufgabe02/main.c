6950195a34fe1c84d8285a80a9bf461065003e73
Stefan Ebert <stefan+git@ebert-linux.de>
2011-10-22 11:44:57 +0200

    ulam_max läuft nun, dadruch auch die anderen sachen

--- aufgabe02/main.c
+++ aufgabe02/main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    printf ("Max: %d\n", ulam_max (0) );
+    printf ("Max: %d\n", ulam_max (7) );
     printf ("Twins: %d\n", ulam_twins (6) );
     printf ("Multiple: %d\n", ulam_multiples (0, 3) );
     return 0;
--- aufgabe02/ulam.c
+++ aufgabe02/ulam.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-int ulam_max(int a0)
+int ulam_max (int a0)
 {
-    int max;
+    int max = -1;
     
-    if (a0 <= 0)
-    {
-        return -1;
-    }
-
-    max  = a0;
-    while (a0 < 1);
+    while (a0 > 1)
     {
         a0 = (a0 % 2 == 0) ? a0 / 2 : 3 * a0 + 1;
         max = (max > a0) ? max : a0;
@@ -18,17 +12,14 @@ int ulam_max(int a0)
     return max;
 }
 
-int ulam_twins(int limit)
+int ulam_twins (int limit)
 {
-    int twin;
+    int twin = -1;
     int i;
-
-    twin = -1;
 
     for (i = 1; i <= limit; i++)
     {
-        printf ("%d %d %d\n", i, ulam_max (i), ulam_max (i + 1) );
-        if (ulam_max (i) == ulam_max (i + 1))
+        if (ulam_max (i) == ulam_max (i + 1) )
         {
             twin = i;
         }
@@ -36,7 +27,7 @@ int ulam_twins(int limit)
     return twin;
 }
 
-int ulam_multiples(int limit, int number)
+int ulam_multiples (int limit, int number)
 {
     return 0;
 }
