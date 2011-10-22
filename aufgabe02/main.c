8f17fd690c13b8e344908b5c2b0b0abe9c0a1ad0
Stefan Ebert <stefan+git@ebert-linux.de>
2011-10-22 18:14:55 +0200

    teställe vom übungsblatt

--- aufgabe02/main.c
+++ aufgabe02/main.c
@@ -3,8 +3,13 @@
 
 int main(void)
 {
-    printf ("Max: %d\n", ulam_max (7) );
-    printf ("Twins: %d\n", ulam_twins (6) );
-    printf ("Multiple: %d\n", ulam_multiples (0, 3) );
+    printf ("ulam_max (5): %d | 16\n", ulam_max (5) );
+    printf ("ulam_max (7): %d | 52\n", ulam_max (7) );
+    printf ("ulam_twins (10): %d | 5\n", ulam_twins (10) );
+    printf ("ulam_twins (5): %d | -1\n", ulam_twins (5) );
+    printf ("ulam_multiple (10, 2): %d | 5\n", ulam_multiples (10, 2) );
+    printf ("ulam_multiple (1000, 3): %d | 972\n", ulam_multiples (1000, 3) );
+    printf ("ulam_multiple (108, 3): %d | -1\n", ulam_multiples (108, 3) );
+    printf ("ulam_multiple (391, 6): %d | 386\n", ulam_multiples (391, 6) );
     return 0;
 }
