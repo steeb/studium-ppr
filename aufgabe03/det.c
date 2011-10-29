24336b8bc5ac63ce55ccde467b4ea4181c9957c9
Stefan Ebert <stefan+git@ebert-linux.de>
2011-10-29 16:08:28 +0200

    fertig kommentiert

--- aufgabe03/det.c
+++ aufgabe03/det.c
@@ -1,11 +1,33 @@
+/**
+ * @file Berechnet Determinanten zu quadratischen Matrizen und kann diese
+ * ausgeben
+ *
+ * @author steeb
+ * @date 2011-10-29
+ */
+
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_SIZE 10
 
+/**
+ * Berechnet die Determinante einer quadratischen Matrix
+ *
+ * @param matrix - Matrix zu der die Determinante berechnet werden soll
+ * @param size - Groesse der Matrix
+ *
+ * @return Determinante der Matrix oder im Fehlerfall 0.0
+ */
 double get_determinant (double matrix[MAX_SIZE][MAX_SIZE], int size)
 {
-    if (size == 1)
+    if (size < 1 || size > MAX_SIZE)
+    {
+        printf ("Size Parameter falsch");
+        return 0.0;
+    }
+    else if (size == 1)
     {
         return matrix[0][0];
     }
@@ -15,13 +37,13 @@ double get_determinant (double matrix[MAX_SIZE][MAX_SIZE], int size)
     }
     else
     {
-        double det = 0;
-        double matrix2[MAX_SIZE][MAX_SIZE];
-        int p;
-        int i;
-        int j;
-        int h;
-        int k;
+        double det = 0; /* Ausgerechnete Determinante */
+        double matrix2[MAX_SIZE][MAX_SIZE]; /* Untermatrix */
+        int p; /* Laufvariable für die Laplaceformel */
+        int i; /* Laufvariable um horizontal über die Matrix zu laufen */
+        int j; /* Laufvariable um vertikal über die Matrix zu laufen */
+        int h; /* Laufvariable um horizontal über die Untermatrix zu laufen */
+        int k; /* Laufvariable um vertikal über die Untermatrix zu laufen */
 
         for (p = 0; p < size; p++)
         {
@@ -49,10 +71,23 @@ double get_determinant (double matrix[MAX_SIZE][MAX_SIZE], int size)
     }
 }
 
+/**
+ * Gibt die üebergebene Matrix auf der stdout aus. Die Zahlen werden mit einer
+ * Nachkommastelle ausgegeben.
+ *
+ * @param matrix - Matrix die ausgegeben werden soll
+ * @param size - Größe der Matrix 
+ */
 void print_matrix (double matrix[MAX_SIZE][MAX_SIZE], int size)
 {
-    int i;
-    int j;
+    int i; /* horizontale Laufvariable */
+    int j; /* vertikale Laufvariable */
+
+    if (size < 0 || size > MAX_SIZE )
+    {
+        printf ("size Paramter flasch");
+        return;
+    }
 
     for (i = 0; i < size; i++)
     {
--- aufgabe03/main.c
+++ aufgabe03/main.c
@@ -1,6 +1,19 @@
+/**
+ * @mainpage Determinanten Berechnung
+ *
+ * Rumspielen mit Determinantenberechnung
+ *
+ *
+ * @author steeb
+ * @date 2011-10-29
+ */
+
 #include <stdio.h>
 #include "det.c"
 
+/** 
+ * funktionen testen
+ */
 int main (void)
 {
     double matrix[][MAX_SIZE] = 
@@ -10,7 +23,14 @@ int main (void)
         {2.0, 8.0, 2.0, 10.0},
         {0.0, 1.0, 1.0, 0.0},
     };
+    double matrix_sarah[][MAX_SIZE] =
+    {
+        {-5.0, -4.0, 0.0, -5.0},
+        {-3.0, -1.0, 4.0, 4.0},
+        {-5.0, 2.0, -5.0, -4.0},
+        {-1.0, -4.0, 3.0, -3.0},
+    };
     print_matrix (matrix, 4);
-    printf ("%f\n", get_determinant (matrix, 4));
+    printf ("%f\n", get_determinant (matrix_sarah, 4));
     return 0;
 }
